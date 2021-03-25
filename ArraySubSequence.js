0fad74693648c3ba6127bac174cb4cc85f50dba9
Esteban Saravia <estebansaravia@gmail.com>
2021-03-25 19:21:16 -0300

    adding description

--- ArraySubSequence.js
+++ ArraySubSequence.js
@@ -1,3 +1,20 @@
+/* 
+
+  Given two non-empty arrays of integers, write a function that determines
+  whether the second array is a subsequence of the first one.
+
+  A subsequence of an array is a set of numbers that aren't necessarily adjacent
+  in the array but that are in the same order as they appear in the array. For
+  instance, the numbers form a subsequence of the array
+  
+
+ [1, 3, 4]form a subsequence of the array [1, 2, 3, 4], and so do the numbers <span>[2, 4]</span>. 
+ Note: that a single number in an array and the array itself are both valid
+  subsequences of the array.
+
+
+  */
+
 function isValidSubsequence(array, sequence) {
   if (array === sequence) {
     return true;

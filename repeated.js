2efddf5445dbd36e58c6a73e9fb1344964dca30c
Esteban Saravia <estebansaravia@gmail.com>
2021-03-25 21:48:01 -0300

    adding more eficient solution to Repeated Words problem

--- repeated.js
+++ repeated.js
@@ -1,4 +1,7 @@
-function repeatedWords(phrase) {
+/** Given a text find the words repeated  */
+
+// Result in a Order Quadratic
+function repeatedWordsQuadraticSolution(phrase) {
   let result = [];
   Array.from(new Set(phrase.split(' '))).forEach((word) => {
     let acc = 1;
@@ -7,7 +10,23 @@ function repeatedWords(phrase) {
   });
   return result;
 }
+// Result in Order Lineal
+function repeateWordsLinealSolution(phrase) {
+  let words = phrase.split(' ');
+  let dic = new Map();
 
-repeatedWords(
-  'tres tristes tigres tres tigres tres tigres tres tigres trigo trillado trigo trillado trigo trillado trigo trillado tragaban en un trigal'
+  for (let word of words) {
+    let nomalizedWord = word.toLowerCase().replace(/[.,!]/g, '');
+    if (dic.has(nomalizedWord)) {
+      let oldValue = dic.get(nomalizedWord);
+      dic.set(nomalizedWord, ++oldValue);
+    } else {
+      dic.set(nomalizedWord, 1);
+    }
+  }
+  console.log(dic);
+}
+
+repeateWordsV2(
+  'tres tristes tigres Tres tigres!. tres, tigres tres tigres trigo trillado trigo trillado trigo trillado trigo trillado tragaban en un trigal'
 );
